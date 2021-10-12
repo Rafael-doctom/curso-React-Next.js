7af5e22eabad985701d9e57329b1c6efaf692141
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-12 19:31:50 -0300

    this update Class

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 
 
-export class App extends Component{
+export class App extends Component {
 
-    state = {
-      contador: 0
-    }
+  state = {
+    contador: 0
+  }
 
-    handleClick = () => {
-      this.setState({contador: this.state.contador + 1})
-    }
-    render(){
-      return(
-        <>
+  handleClick = () => {
+    this.setState({ contador: this.state.contador + 1 })
+  }
+  render() {
+    return (
+      <>
         <div>
           <p>total: {this.state.contador}</p>
           <button onClick={this.handleClick}>add mais 1</button>
         </div>
-        </>
-      )
-    }
+      </>
+    )
+  }
 }
 
 export default App
