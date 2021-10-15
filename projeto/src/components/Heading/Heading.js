cc03962d8510284c864a52ef8843b94fcbe4ec8b
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-15 15:52:10 -0300

    -- update component

---
+++ projeto/src/components/Heading/Heading.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+
+function Heading() {
+    return (
+        <>
+            <div className="Container">
+
+                <div className="ProfileImage">
+                    LOGO
+                </div>
+
+                <div className="NavList">
+                    <ul>
+                        <li>
+                            intro
+                        </li>
+                        <li>
+                            grid-one
+                        </li>
+                        <li>
+                            gallery
+                        </li>
+                        <li>
+                            grid-two
+                        </li>
+                        <li>
+                            pricing
+                        </li>
+                        <li>
+                            contact
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </>
+    )
+};
+
+export default Heading
