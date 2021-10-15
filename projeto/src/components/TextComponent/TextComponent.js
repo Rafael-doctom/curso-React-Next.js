4d5906d5f7872b24e7b866366409aa570a8d8365
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-15 15:52:28 -0300

    -- update component

---
+++ projeto/src/components/TextComponent/TextComponent.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function TextComponent() {
+    return (
+        <>
+            <div className="Container">
+                <div className="Content">
+                    <div className="TextContent">
+                        <div className="TextUpper">
+                            JANUARY BRINGS US FIREFOX 85
+                        </div>
+
+                        <div className="TextLower">
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat laborum."
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default TextComponent
